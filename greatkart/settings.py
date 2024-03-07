3801e6d14395c8f70a01e6eaeb2b1f3cb04cd3ea
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2024-03-07 21:51:03 +0100

    new commit

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['172.31.19.115']
+ALLOWED_HOSTS = ["34.215.140.235", "django-greatcart-env.eba-jpkhupy5.us-west-2.elasticbeanstalk.com"]
 # ALLOWED_HOSTS = ['*']
 
 
