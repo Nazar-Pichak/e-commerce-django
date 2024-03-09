39f7fabed37277c0091f0de867f87235248169d4
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2024-03-09 19:57:36 +0100

    changes for bucket

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ["172.31.19.115", "django-greatcart-env.eba-jpkhupy5.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["172.31.19.115", "django-greatcart-env.eba-jpkhupy5.us-west-2.elasticbeanstalk.com", '*']
 # ALLOWED_HOSTS = ['*']
 
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -152,14 +153,38 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'greatkart/static',
+    'media',
 ]
+# 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = 'public-read'
+
+AWS_S3_REGION_NAME = 'us-west-2'
+CORS_ALLOW_ALL_ORIGINS = True
+AWS_LOCATION = 'static'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'greatkart.media_storage.MediaStorage'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+
+
 
 from django.contrib.messages import constants as messages
 
