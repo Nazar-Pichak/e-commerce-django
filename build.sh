d3cde64be0189fd171ebbe411d4964eb43013781
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2025-03-06 22:14:17 +0100

    migrate app to vercel

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Exit on error
 
+python3 -m venv venv
+source venv/bin/activate
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install --upgrade pip
 pip install -r requirements.txt
