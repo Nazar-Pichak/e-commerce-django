07328dd51ec34b48e74f79b2dc94678e27ab3569
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2025-03-07 01:01:48 +0100

    migrate app to vercel

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
